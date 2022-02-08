f003d950f5fbd1a8b9e1afd3e5d270b623770d3f
rafal.zarajczyk <rafal.zarajczyk@allegro.pl>
2022-02-08 22:44:37 +0100

    - using date in docker-build.sh  - changed default log extenstion to .txt

--- docker-build.sh
+++ docker-build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-TAG=$1
-if [ -z $TAG ]; then
-    echo "TAG is required"
-    exit 1
-fi
-
+TAG=$(date '+%Y%m%d')
 docker build -t rzarajczyk/smart-devices-to-graphite:$TAG .
 docker tag rzarajczyk/smart-devices-to-graphite:$TAG rzarajczyk/smart-devices-to-graphite:latest
