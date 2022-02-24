c11a1079a354b98140e2e4f8ad959b7fcf0be9b3
rafal.zarajczyk <rafal.zarajczyk@allegro.pl>
2022-02-24 16:16:58 +0100

    Better naming

--- src/main/devices/PhilipsHue.py
+++ src/main/devices/PhilipsHue.py
@@ -98,7 +98,7 @@ class PhilipsHue(Device_Base):
                                             set_handler=handler)
             self.properties_bri[group_id] = property_bri
             add_property_string(self, 'brightness-transition',
-                                property_name="Brightness - start transition",
+                                property_name="Smooth dim",
                                 parent_node_id=homie_group_id,
                                 retained=False,
                                 set_handler=handler)
@@ -116,7 +116,7 @@ class PhilipsHue(Device_Base):
                                            set_handler=handler)
             self.properties_ct[group_id] = property_ct
             add_property_string(self, 'color-temperature-transition',
-                                property_name="Color Temperature - start transition",
+                                property_name="Smooth Color Temperature change",
                                 parent_node_id=homie_group_id,
                                 retained=False,
                                 set_handler=handler)
