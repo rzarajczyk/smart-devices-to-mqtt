3fd3404826cbf886c7bbc32bfb9c2e3cb9507bac
rafal.zarajczyk <rafal.zarajczyk@allegro.pl>
2022-02-25 23:56:32 +0100

    Removed IP

--- docker-run.sh
+++ docker-run.sh
@@ -3,7 +3,6 @@ TMP=$(mktemp -d)
 cp $(pwd)/config/smart-devices-to-mqtt.yaml $TMP
 echo "Temp directory is $TMP"
 docker run -it --rm  \
-    --add-host "mosquitto:192.168.86.159" \
     --name smart-devices-to-mqtt \
     -v $TMP:/smart-devices-to-mqtt/config \
     rzarajczyk/smart-devices-to-mqtt:latest
