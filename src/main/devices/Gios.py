700c44dbfc26c673dfebf270f86f8ba906a7600c
rafal.zarajczyk <rafal.zarajczyk@allegro.pl>
2022-03-01 16:55:00 +0100

    Gios

--- src/main/devices/Gios.py
+++ src/main/devices/Gios.py
@@ -4,26 +4,28 @@ import requests
 from apscheduler.schedulers.base import BaseScheduler
 from homie.device_base import Device_Base
 
-from homie_helpers import add_property_float, add_property_string, add_property_boolean
+from homie_helpers import add_property_float
 
 
 class Gios(Device_Base):
     def __init__(self, device_id, config, mqtt_settings, scheduler: BaseScheduler):
         super().__init__(device_id=device_id, name="GIOS AirQuality data", mqtt_settings=mqtt_settings)
 
+        self.NORMS = {
+            'NO2': [40, 100, 150, 230, 400],
+            'PM10': [20, 50, 80, 110, 150],
+            'PM2.5': [13, 35, 55, 75, 110]
+        }
+        self.NORMS_READABLE = ['Bardzo dobry', 'Dobry', 'Umiarkowany', 'Dostateczny', 'Zły', 'Bardzo zły']
+
         self.station_id = config['station-id']
 
         self.properties = {
-            "C6H6": add_property_float(self, 'c6h5-value', property_name="Benzen (C6H6)", parent_node_id="data", unit=None),
-            "CO": add_property_float(self, 'co-value', property_name="Tlenek węgla (CO)", parent_node_id="data", unit=None),
-            "NO2": add_property_float(self, 'no2-value', property_name="Dwutlenek azotu (NO2)", parent_node_id="data", unit=None),
-            "PM10": add_property_float(self, 'pm10-value', property_name="Pył zawieszony PM 10", parent_node_id="data", unit=None),
-            "PM2.5": add_property_float(self, 'pm25-value', property_name="Pył zawieszony PM 2.5", parent_node_id="data", unit=None),
-            "date_C6H6": add_property_string(self, 'c6h5-date', property_name="Benzen (C6H6) - data pomiaru", parent_node_id="data", unit=None),
-            "date_CO": add_property_string(self, 'co-date', property_name="Tlenek węgla (CO) - data pomiaru", parent_node_id="data", unit=None),
-            "date_NO2": add_property_string(self, 'no2-date', property_name="Dwutlenek azotu (NO2) - data pomiaru", parent_node_id="data", unit=None),
-            "date_PM10": add_property_string(self, 'pm10-date', property_name="Pył zawieszony PM 10 - data pomiaru", parent_node_id="data", unit=None),
-            "date_PM2.5": add_property_string(self, 'pm25-date', property_name="Pył zawieszony PM 2.5 - data pomiaru", parent_node_id="data", unit=None),
+            "C6H6": add_property_float(self, 'c6h5', property_name="Benzen (C6H6)", parent_node_id="data", unit=None),
+            "CO": add_property_float(self, 'co', property_name="Tlenek węgla (CO)", parent_node_id="data", unit=None),
+            "NO2": add_property_float(self, 'no2', property_name="Dwutlenek azotu (NO2)", parent_node_id="data", unit=None),
+            "PM10": add_property_float(self, 'pm10', property_name="Pył zawieszony PM 10", parent_node_id="data", unit=None),
+            "PM2.5": add_property_float(self, 'pm25', property_name="Pył zawieszony PM 2.5", parent_node_id="data", unit=None),
         }
 
         scheduler.add_job(self.refresh, 'interval', seconds=config['fetch-interval-seconds'], next_run_time=datetime.now())
@@ -40,4 +42,22 @@ class Gios(Device_Base):
             last_value = data['values'][0]['value']
             last_date = data['values'][0]['date']
             self.properties[sensor_code].value = last_value
-            self.properties["date_%s" % sensor_code].value = last_date
+            self.properties[sensor_code].meta['measurement-date'] = {'name': 'measurement-date', 'value': last_date}
+            self.properties[sensor_code].meta['description'] = {'name': 'description', 'value': self.describe(sensor_code, last_value)}
+            self.properties[sensor_code].publish_meta()
+
+    def describe(self, code, value):
+        if code not in self.NORMS:
+            # return -1, ''
+            return ''
+        i = 0
+        norms = self.NORMS[code]
+        while i < len(norms):
+            l = norms[i - 1] if i > 0 else -1
+            r = norms[i]
+            if l <= value < r:
+                # return i, self.NORMS_READABLE[i]
+                return self.NORMS_READABLE[i]
+            i += 1
+        # return len(norms), self.NORMS_READABLE[len(norms)]
+        return self.NORMS_READABLE[len(norms)]
