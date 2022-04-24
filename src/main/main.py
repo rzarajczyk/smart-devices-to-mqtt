74b09030f0d25473eb65a1f4a3fe753aa4585e3c
Rafal Zarajczyk <rzarajczyk@box.com>
2022-04-24 20:14:18 +0200

    removing XiaomiDeskLight.py

--- src/main/main.py
+++ src/main/main.py
@@ -12,7 +12,20 @@ from devices.XiaomiAirPurifier import XiaomiAirPurifier
 from devices.XiaomiAirQualityMonitor import XiaomiAirQualityMonitor
 ########################################################################################################################
 # logging configuration
-from devices.XiaomiDeskLight import XiaomiDeskLight
+from devices.XiaomiDeskLight impimport logging
+from logging import config as logging_config
+
+import yaml
+from apscheduler.schedulers.blocking import BlockingScheduler
+from homie_helpers import HomieSettings
+
+from devices.PhilipsHue import PhilipsHue
+from devices.PrinterScanner import PrinterScanner
+from devices.XiaomiAirHumidifier import XiaomiAirHumidifier
+from devices.XiaomiAirPurifier import XiaomiAirPurifier
+from devices.XiaomiAirQualityMonitor import XiaomiAirQualityMonitor
+########################################################################################################################
+# logging configuration
 
 with open("logging.yaml", 'r') as f:
     config = yaml.full_load(f)
@@ -44,7 +57,63 @@ DEVICE_CLASSES = {
     'xiaomi-air-monitor': XiaomiAirQualityMonitor,
     'xiaomi-air-purifier': XiaomiAirPurifier,
     'xiaomi-air-humidifier': XiaomiAirHumidifier,
-    'xiaomi-desk-light': XiaomiDeskLight,
+    'philips-hue': PhilipsHue,
+    'printer-scanner': PrinterScanner
+}
+
+LOGGER.info('Registered device classes:')
+for device_class_name in DEVICE_CLASSES:
+    LOGGER.info(' - %s : %s' % (device_class_name, str(DEVICE_CLASSES[device_class_name])))
+
+scheduler = BlockingScheduler(timezone="Europe/Warsaw")
+
+DEVICES = []
+
+for device_id in DEVICES_CONFIG:
+    device_config = DEVICES_CONFIG[device_id]
+    device_type = device_config['type']
+    device_class = DEVICE_CLASSES[device_type]
+    LOGGER.info('Creating new device ≪%s≫ of type ≪%s≫' % (device_id, device_type))
+    device = device_class(device_id, device_config, MQTT_SETTINGS, scheduler)
+    DEVICES.append(device)
+
+LOGGER.info('All created devices:')
+for device in DEVICES:
+    LOGGER.info(' - %s' % str(device))
+
+scheduler.start()
+ort XiaomiDeskLight
+
+with open("logging.yaml", 'r') as f:
+    config = yaml.full_load(f)
+    logging_config.dictConfig(config)
+
+LOGGER = logging.getLogger("main")
+LOGGER.info("Starting application!")
+
+########################################################################################################################
+# application configuration
+
+with open('config/smart-devices-to-mqtt.yaml', 'r') as f:
+    config = yaml.full_load(f)
+
+    MQTT_SETTINGS = HomieSettings(
+        broker=config['mqtt']['host'],
+        port=config['mqtt']['port'],
+        username=config['mqtt']['user'],
+        password=config['mqtt']['password']
+    )
+
+    DEVICES_CONFIG = config['devices']
+
+########################################################################################################################
+# core logic
+
+
+DEVICE_CLASSES = {
+    'xiaomi-air-monitor': XiaomiAirQualityMonitor,
+    'xiaomi-air-purifier': XiaomiAirPurifier,
+    'xiaomi-air-humidifier': XiaomiAirHumidifier,
     'philips-hue': PhilipsHue,
     'printer-scanner': PrinterScanner
 }
